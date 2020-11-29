6f5790c22c6b184ee88c555dfa39db891eb3dc93
Magda <magdalena.z_21@interia.pl>
2020-11-29 21:20:33 +0100

    Variables

--- kodilla-intro/src/main/java/MainExtended.java
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class MainExtended {
+public class MainExtended {
+    public static void main(String[] args) {
+        String myName = "Magda";
+        int myAge = 24;
+        char drivingLicense = 'B';
+        boolean hasAnimals = true;
+        boolean smokes = false;
+        int shoeNumber = 39;
+        System.out.println(myName);
+        System.out.println(myAge);
+        System.out.println(drivingLicense);
+        System.out.println(hasAnimals);
+        System.out.println(smokes);
+        System.out.println(shoeNumber);
+    }
 }

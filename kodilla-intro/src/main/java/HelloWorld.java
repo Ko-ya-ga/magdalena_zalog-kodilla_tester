fc8dc214e0c81c67e76ce4842bebad98638fed5c
Magda <magdalena.z_21@interia.pl>
2020-11-29 20:07:21 +0100

    Java Class

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {                      //1
+    public static void main(String[] args) {   //2
+        System.out.println("Hello World!");    //3
+        System.out.println("Magda");            //4
+        System.out.println(24);                 //5
+        System.out.println('z');                //6
+    }
+}

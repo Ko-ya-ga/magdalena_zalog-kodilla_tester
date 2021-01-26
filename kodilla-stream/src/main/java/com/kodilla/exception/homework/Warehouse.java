31dabf09dbe4f84ae1b9fcfd9238df60c037bd86
Magda <magdalena.z_21@interia.pl>
2021-01-26 22:31:54 +0100

    Exceptions

---
+++ kodilla-stream/src/main/java/com/kodilla/exception/homework/Warehouse.java
@@ -0,0 +1,25 @@
+package com.kodilla.exception.homework;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class Warehouse {
+    static List<Order> orders = new ArrayList<>();
+
+    public static void addOrder(Order order) {
+        orders.add(order);
+        System.out.println(orders);
+    }
+
+    public static Order getOrder(String number) throws OrderDoesntExistException {
+        Optional<Order> result = orders
+                .stream()
+                .filter(o -> o.getNumber().equals(number))
+                .findAny();
+        if (result.isEmpty())
+            throw new OrderDoesntExistException();
+        return  result.get();
+    }
+}
---
+++ kodilla-stream/src/main/java/com/kodilla/exception/homework/WarehouseApp.java
@@ -0,0 +1,18 @@
+package com.kodilla.exception.homework;
+
+public class WarehouseApp {
+    public static void main(String[] args) {
+        Warehouse.addOrder(new Order("1657885"));
+        Warehouse.addOrder(new Order("2290339"));
+        Warehouse.addOrder(new Order("3758302"));
+        Warehouse.addOrder(new Order("4573819"));
+        Warehouse.addOrder(new Order("5043958"));
+
+        try {
+            Order isOrderPresent = Warehouse.getOrder("9875654");
+            System.out.println("Order status: " + isOrderPresent);
+        } catch (OrderDoesntExistException e) {
+            System.out.println("Sorry, this order doesn't exist");
+        }
+    }
+}
